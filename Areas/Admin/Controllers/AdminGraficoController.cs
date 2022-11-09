f87a30f0104755bfc5a6b0855bc787a61d674e07
renatoas77 <renato.antonio7@hotmail.com>
2022-11-09 20:19:30 -0300

    Começando a implementar a função de gerar graficos

---
+++ Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,38 @@
+﻿using LanchesMac.Areas.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    public class AdminGraficoController : Controller
+    {
+        private readonly GraficoVendaService _graficoVendas;
+
+        public AdminGraficoController(GraficoVendaService graficoVendas)
+        {
+            _graficoVendas = graficoVendas ?? throw new ArgumentNullException(nameof(graficoVendas));
+        }
+
+        public JsonResult VendasLanches(int dias)
+        {
+            return Json(_graficoVendas.GetVendasLanches(dias));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VendasMensais()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VendasSemanais()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Admin/Services/GraficoVendaService.cs
@@ -0,0 +1,43 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+
+namespace LanchesMac.Areas.Admin.Services
+{
+    public class GraficoVendaService
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public GraficoVendaService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<LancheGrafico> GetVendasLanches(int dias = 360)
+        {
+            var data = DateTime.Now.AddDays(-dias);
+
+            var lanches =  (from pd in _appDbContext.PedidoDetalhes
+                           join l in _appDbContext.Lanches
+                           on pd.LancheId equals l.LancheId
+                           where pd.Pedido.PedidoEnviado >= data
+                           group pd by new { pd.LancheId, l.Nome, pd.Quantidade } into g
+                           select new
+                           {
+                               LancheNome= g.Key.Nome,
+                               LanchesQuantidade = g.Sum(q => q.Quantidade),
+                               ValorTotalLanche = g.Sum(a => a.Preco * a.Quantidade),
+                           });
+
+            var lista = new List<LancheGrafico>();
+            foreach (var item in lanches)
+            {
+                var lanche = new LancheGrafico();
+                lanche.LancheNome = item.LancheNome;
+                lanche.LanchesQuantidade = item.LanchesQuantidade;
+                lanche.ValorTotalLanche = item.ValorTotalLanche; 
+                lista.Add(lanche);
+            }
+            return lista;
+        }
+    }
+}
